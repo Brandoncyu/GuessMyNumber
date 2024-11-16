5f6cbde1e1dfaff6abe109ee90de2086b5548945
brandoncyu <Brandoncyu@gmail.com>
2024-11-15 18:46:57 -0800

    finished project

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import Colors from './constants/colors';
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0)
 
   const [fontsLoaded] = useFonts({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
@@ -19,7 +20,7 @@ export default function App() {
   })
 
   if (!fontsLoaded){
-    return <AppLoad />
+    return <AppLoading />
   }
 
   function pickedNumberHandler(pickedNumber){
@@ -27,8 +28,15 @@ export default function App() {
     setGameIsOver(false)
   }
 
-  function gameOverHandler(){
+  function gameOverHandler(numberOfRounds){
     setGameIsOver(true)
+    setGuessRounds(numberOfRounds)
+  }
+
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGameIsOver(true);
+    setGuessRounds(0)
   }
 
   let screen = <StartGameScreen  onConfirmNumber={pickedNumberHandler}/>
@@ -38,7 +46,11 @@ export default function App() {
   }
 
   if (gameIsOver && userNumber){
-    screen = <GameOverScreen/>
+    screen = <GameOverScreen 
+      userNumber={userNumber} 
+      roundsNumber={guessRounds} 
+      onStartNewGame={startNewGameHandler} 
+    />
   }
 
   return (
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native"
+import { View, Text, StyleSheet, Alert, FlatList } from "react-native"
 import { Ionicons } from '@expo/vector-icons'
 
 import NumberContainer from "../components/game/NumberContainer";
@@ -7,6 +7,7 @@ import Title from '../components/ui/Title'
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
+import GuessLogItem from "../components/game/GuessLogItem";
 
 function generateRandomBetween(min, max, exclude) {
     const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -24,12 +25,18 @@ let maxBoundary = 100
 function GameScreen({userNumber, onGameOver}) {
     const initialGuess = generateRandomBetween(1, 100, userNumber)
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    const [guessRounds, setGuessRounds] = useState([initialGuess])
 
     useEffect(()=>{
         if (currentGuess == userNumber){
-            onGameOver();
+            onGameOver(guessRounds.length);
         }
     }, [currentGuess, userNumber, onGameOver])
+
+    useEffect(() => {
+        minBoundary = 1;
+        maxBoundary = 100
+    }, []);
 
     function nextGuessHandler(direction){ //direction => 'lower', 'greater'
         if (
@@ -49,7 +56,10 @@ function GameScreen({userNumber, onGameOver}) {
         }
         const newRandomNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
         setCurrentGuess(newRandomNumber);
+        setGuessRounds(prevGuessRounds => [newRandomNumber, ...prevGuessRounds])
     }
+
+    const guessRoundsListLength = guessRounds.length;
 
     return (
         <View style={styles.screen}>
@@ -70,9 +80,18 @@ function GameScreen({userNumber, onGameOver}) {
                     </View>
                 </View>
             </Card>
-            {/* <View> */}
-                {/* LOG ROUNDS */}
-            {/* </View> */}
+            <View style={styles.listContainer}>
+                <FlatList
+                    data={guessRounds}
+                    renderItem={(itemData) => (
+                        <GuessLogItem 
+                            roundNumber={guessRoundsListLength - itemData.index} 
+                            guess={itemData.item} 
+                        />
+                    )}
+                    keyExtractor={(item) => item}
+                />
+            </View>
         </View>
     )
 }
@@ -92,5 +111,9 @@ const styles = StyleSheet.create({
     },
     buttonContainer: {
         flex: 1
+    },
+    listContainer: {
+        flex: 1,
+        padding: 16
     }
 })
